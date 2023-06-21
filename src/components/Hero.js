fe6279f649c9f15da4714275f823b7504dd1517f
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-06-21 22:10:07 +0100

    Second commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styles from "./Hero.module.css";
 
 function Hero() {
   return (
-    <div>
-      <p>Hero</p>
+    <div className={styles.hero}>
+      <img className={styles.logopic} src="screen.png" alt="logo" />
+      <h1 className={styles.title}>Online Experiences</h1>
+      <h2 className={styles.subtitle}>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </h2>
     </div>
   );
 }
